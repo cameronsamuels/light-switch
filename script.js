44c9177def05c8f9420a36648ee5ee7713b6b429
Cameron Samuels <me@cameronsamuels.com>
2018-07-25 17:08:38 -0500

    (initial)Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+  
+  document.querySelector("input[type='checkbox']").addEventListener("change", function() {
+    
+    var checked = this.checked;
+    if (checked) {
+      document.querySelector("div>div").style.transform = "scaleY(-1)";
+      document.body.style.background = "#f6dfdf";
+    }
+    else {
+      document.querySelector("div>div").style.transform = "";
+      document.body.style.background = "";
+    }
+    
+  });
+  
+});
