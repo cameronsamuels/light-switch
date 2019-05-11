9b14050eef8280a61b2ba71a217f9441f1031d5c
Cameron Samuels <me@cameronsamuels.com>
2019-05-10 20:15:36 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
-  
   document.querySelector("input[type='checkbox']").addEventListener("change", function() {
-    
-    var checked = this.checked;
-    if (checked) {
+    if (this.checked) {
       document.querySelector("div>div").style.transform = "scaleY(-1)";
       document.body.style.background = "#f6dfdf";
     }
@@ -11,7 +8,5 @@ document.addEventListener("DOMContentLoaded", function() {
       document.querySelector("div>div").style.transform = "";
       document.body.style.background = "";
     }
-    
   });
-  
 });
