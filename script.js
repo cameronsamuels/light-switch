73afc0e9e033cb3a76058838cd814d53ef2b3c9d
Cameron Samuels <54650557+cameronsamuels@users.noreply.github.com>
2020-06-06 00:54:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ document.addEventListener("DOMContentLoaded", function() {
     if (this.checked) {
       document.querySelector("div>div").style.transform = "scaleY(-1)";
       document.body.style.background = "#f6dfdf";
+      document.querySelector("a").style.display = "block";
     }
     else {
       document.querySelector("div>div").style.transform = "";
       document.body.style.background = "";
+      document.querySelector("a").style.display = "";
     }
   });
 });
